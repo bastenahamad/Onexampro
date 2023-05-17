802ae3e65fdf6f9f3ba97c9894e8e94461656ef1
mandeep-mahra <mandeepmehara1@gmail.com>
2023-05-17 22:48:39 +0530

    qbut

--- backend/info_bar.js
+++ backend/info_bar.js
@@ -5,6 +5,7 @@
 for(var i=0; i<10; i++) {
     navbut[i] = document.createElement("BUTTON");
     navbut[i].innerHTML = i+1;
+    navbut[i].className = "infobtn"
     document.querySelector(".questionlist").appendChild(navbut[i]);
 }
 
