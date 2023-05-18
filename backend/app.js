5576d1e6dbad54230db5084574ff4c7dc99471b9
mandeep-mahra <mandeepmehara1@gmail.com>
2023-05-18 14:40:07 +0530

    check

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 
 const {MongoClient} = require('mongodb');
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(url);
 
 const dbName = 'examination';
